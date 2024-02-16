f7190ff77edf94490c53639ee661f298a4beb5b4
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-02-16 03:09:50 +0200

    edit gitignore

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterUserView, LoginUserView, PasswordResetRequestView, PasswordResetConfirm, SetNewPasswordView
+from .views import RegisterUserView, LoginUserView, PasswordResetRequestView, PasswordResetConfirm, SetNewPasswordView, LogoutApiView
 
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name='reset-password-confirm'),
     path('set-new-password/', SetNewPasswordView.as_view(), name='set-new-password'),
+    path('logout/', SetNewPasswordView.as_view(), name='loguot'),
 ]
