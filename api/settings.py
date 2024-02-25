a7d6071dffcf3b76fea4da96fc44ae8aacc5d46c
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-02-25 21:05:23 +0200

    updating python

--- api/settings.py
+++ api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*', 'http://localhost:3000']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
