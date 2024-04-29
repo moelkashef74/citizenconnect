6a99b013af4e69d291c53d324735a65a8498222e
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-04-29 23:29:56 +0300

    adding user to all the report tables

--- reports3/serializers.py
+++ reports3/serializers.py
@@ -4,6 +4,13 @@ from .models import Report_cat_three
 
 
 class ReportSerializer(serializers.ModelSerializer):
+
+    user = serializers.SerializerMethodField(method_name='get_user_email')
+
     class Meta:
         model = Report_cat_three
-        fields = ['id','image', 'description', 'location', 'status', 'created_at']
+        fields = ['id','image', 'description', 'location', 'status', 'created_at','user']
+
+        
+    def get_user_email(self, obj):
+        return obj.user.get_full_name
