ad3fc0687baf0326f8b39c9d04e21e8d8fad7a27
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-06-19 19:51:22 +0300

    using vercel

--- api/settings.py
+++ api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['citizenconnectt.pythonanywhere.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['citizenconnectt.pythonanywhere.com','127.0.0.1','localhost', '.vercel.app']
 
 
 # Application definition
