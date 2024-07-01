5a7d35371bd1f37ce9e6f69cbf12c0600f88bf0d
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-07-01 04:27:21 +0300

    traffic to road

--- reports/views.py
+++ reports/views.py
@@ -173,7 +173,7 @@ class Cat3TimelineView(ListAPIView):
     serializer_class = ReportSerializer
 
 class OtherTimelineView(ListAPIView):
-    queryset = Report.objects.filter(status="solved").exclude(category__in=["cat1", "cat2", "cat3"]).order_by('-created_at')
+    queryset = Report.objects.filter(category="Other", status="solved").order_by('-created_at')
     serializer_class = ReportSerializer
 
 
