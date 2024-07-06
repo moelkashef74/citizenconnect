8658bb91096cec792e3e3cff5cbfb99750f15992
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-07-06 20:35:36 +0300

    add notification

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,9 +5,8 @@ from rest_framework.exceptions import AuthenticationFailed
 from django.utils.encoding import smart_str, force_str, smart_bytes
 
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-
+from django.utils import timezone
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.hashers import check_password
 from django.urls import reverse
