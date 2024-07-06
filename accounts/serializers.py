90dfce9671f47fef5953ba63c1ec348778f9e1e4
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-07-06 22:24:08 +0300

    c

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,7 +3,7 @@ from .models import User
 from django.contrib.auth import authenticate
 from rest_framework.exceptions import AuthenticationFailed
 from django.utils.encoding import smart_str, force_str, smart_bytes
-
+from django.core.exceptions import ValidationError
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import timezone
