552504ad06189c6bae22b8f118921626a7886c3d
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-07-07 05:19:11 +0300

    c

--- api/settings.py
+++ api/settings.py
@@ -95,10 +95,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=env('DATABASE_URL'))
         #
         # 'ENGINE': 'django.db.backends.postgresql',
         # 'URL': env('POSTGRES_URL'),
