08522c61d67570ea7f2f68885ea7f67c3c5e1c49
moelkashef74 <20912020100956@fci.zu.edu.eg>
2024-07-09 05:15:45 +0300

    c

--- reports/models.py
+++ reports/models.py
@@ -33,6 +33,8 @@ class Report(models.Model):
         return self.description
 
     def save(self, *args, **kwargs):
+        self.description = smart_str(self.description)
+        self.location = smart_str(self.location)
         if not self.id:
             while True:
                 random_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
